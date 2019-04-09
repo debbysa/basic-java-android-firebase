076e9d599b25856263966238149314604211b0bb
debbysa <debbysa@github.com>
2019-04-09 11:19:31 +0700

    add simple android firebase

---
+++ app/src/main/java/com/deby/firebasecrud/models/DataMahasiswa.java
@@ -0,0 +1,52 @@
+package com.deby.firebasecrud.models;
+
+public class DataMahasiswa {
+    //Deklarasi Variable
+    private String nim;
+    private String nama;
+    private String jurusan;
+    private String key;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+
+    //Membuat Konstuktor kosong untuk membaca data snapshot
+    public DataMahasiswa(){
+    }
+
+    //Konstruktor dengan beberapa parameter, untuk mendapatkan Input Data dari User
+    public DataMahasiswa(String nim, String nama, String jurusan) {
+        this.nim = nim;
+        this.nama = nama;
+        this.jurusan = jurusan;
+    }
+}
